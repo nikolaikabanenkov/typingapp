335e754f7cc0c0999ff7b22bb918bae54be59380
nikolaikabanenkov <58106770+nikolaikabanenkov@users.noreply.github.com>
2022-05-19 21:51:14 +0300

    Добавлены вывод потраченного времени, чтение из файла

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
-// TODO: таймеры, аргументы командной строки, рефактор мейна, чтение из файла
-
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"github.com/nsf/termbox-go"
+	"io/ioutil"
 	"os"
+	"time"
 )
 
 const typedColor = termbox.ColorGreen
@@ -44,24 +44,56 @@ func printStr(x, y int, str string) {
 	}
 }
 
-func colorSegment(x, y, len int, color termbox.Attribute) {
+func paintSegment(x, y, len int, color termbox.Attribute) {
 	for i := 0; i < len; i++ {
 		termbox.SetFg(x+i, y, color)
 	}
 }
 
+func printAndPaint(width, height int, str string, strpos int, typo bool) {
+	leftMargin := width / 4
+	typed, typedMargin, toBeTyped := makeSubstrs(str, strpos, width, leftMargin)
+	printStr(typedMargin, height/2, typed)
+	paintSegment(0, height/2, leftMargin, typedColor)
+	printStr(leftMargin, height/2, toBeTyped)
+	if typo {
+		paintSegment(leftMargin, height/2, width-leftMargin, errorColor)
+	} else {
+		paintSegment(leftMargin, height/2, width-leftMargin, toBeTypedColor)
+	}
+	termbox.SetCursor(leftMargin, height/2)
+}
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: \"./typingapp file\", where \"file\" contains the text you want to type.")
+		return
+	}
+
 	err := termbox.Init()
 	if err != nil {
 		fmt.Println("Couldn't initialize \"termbox\" library.")
 		return
 	}
 
-	str := "The quick brown fox jumps over the lazy dog"
+	bytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Couldn't open file \"", os.Args[1], "\".")
+		return
+	}
+
+	str := string(bytes)
 	strpos := 0
 
+	if len(str) == 0 {
+		fmt.Println("The file is empty!")
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin)
 	typo := false
+	typosNum := 0
+	start := time.Now()
 
 	for strpos != len(str) {
 		width, height := termbox.Size()
@@ -72,29 +104,31 @@ func main() {
 
 		err := termbox.Clear(termbox.ColorWhite, termbox.ColorBlack)
 		if err != nil {
+			fmt.Println("Couldn't clear the console.")
 			return
 		}
 
-		leftMargin := width / 4
-		typed, typedMargin, toBeTyped := makeSubstrs(str, strpos, width, leftMargin)
-		printStr(typedMargin, height/2, typed)
-		colorSegment(0, height/2, leftMargin, typedColor)
-		printStr(leftMargin, height/2, toBeTyped)
-		if typo {
-			colorSegment(leftMargin, height/2, width-leftMargin, errorColor)
-		} else {
-			colorSegment(leftMargin, height/2, width-leftMargin, toBeTypedColor)
-		}
-		termbox.SetCursor(leftMargin, height/2)
+		printAndPaint(width, height, str, strpos, typo)
 
-		termbox.Flush()
+		err = termbox.Flush()
+		if err != nil {
+			fmt.Println("Couldn't flush the buffer.")
+			return
+		}
 
 		ch, _, _ := reader.ReadRune()
 		if ch != rune(str[strpos]) {
 			typo = true
+			typosNum++
 			continue
 		}
+
 		typo = false
 		strpos++
 	}
+
+	end := time.Now()
+
+	fmt.Printf("Your average speed is: %.2f characters per second.\n", float64(len(str))/end.Sub(start).Seconds())
+	fmt.Println("You mamde ", typosNum, " typos.")
 }
